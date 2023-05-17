da14f9ffeee2ee23a4e60f7c05e2000806ebda08
Demis Russo <demisjunior@hotmail.com>
2023-05-16 21:56:44 -0300

    chore: add knex types and create a migration

---
+++ src/@types/knex.d.ts
@@ -0,0 +1,12 @@
+import { Knex } from 'knex'
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    users: {
+      id: string
+      email: string
+      password: number
+      created_at: string
+    }
+  }
+}
--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,7 @@
-import { knex as setupKnex } from 'knex'
+import { knex as setupKnex, Knex } from 'knex'
 import { env } from './env'
 
-export const config = {
+export const config: Knex.Config = {
   client: 'sqlite3',
   connection:
     env.DATABASE_CLIENT === 'sqlite'
