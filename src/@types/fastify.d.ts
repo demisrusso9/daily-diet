12d742758fe6945d87c09d6a561ecead89757f72
Demis Russo <demisjunior@hotmail.com>
2023-06-04 13:52:26 -0300

    refactor: remove route and add type to JwtPayload

--- src/@types/fastify.d.ts
+++ src/@types/fastify.d.ts
@@ -1,8 +1,13 @@
 import fastify from 'fastify'
 import { JwtPayload } from 'jsonwebtoken'
 
+interface User {
+  id: string
+  email: string
+}
+
 declare module 'fastify' {
   export interface FastifyRequest {
-    user: string | JwtPayload | undefined
+    user: string | JwtPayload<User> | undefined
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,5 @@
 import { app } from './app'
 
-app.get('/', (req, res) => {
-  return res.send()
-})
-
 app.listen({ port: 3333 }, () => {
   console.log('listening')
 })
