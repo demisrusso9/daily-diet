5f0e81219915a5ee776b1fb55e55ef416f989c97
Demis Russo <demisjunior@hotmail.com>
2023-06-09 20:24:21 -0300

    feat: add summary route

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from 'fastify'
 import { checkIfTokenExists } from '../middlewares/check-if-token-exists'
 import { knex } from '../database'
 import { z } from 'zod'
-import { OK } from '../utils/statusCode'
+import { INTERNAL_SERVER_ERROR, OK } from '../utils/statusCode'
 
 export async function mealsRoutes(app: FastifyInstance) {
   app.post('/create', { preHandler: checkIfTokenExists }, async (req, reply) => {
@@ -25,8 +25,51 @@ export async function mealsRoutes(app: FastifyInstance) {
   })
 
   app.get('/', { preHandler: checkIfTokenExists }, async (req, reply) => {
-    const meals = await knex('meals').select().where({ user_id: req.user?.id })
+    try {
+      const meals = await knex('meals').select().where({ user_id: req.user?.id })
 
-    return reply.code(OK).send({ result: meals })
+      return reply.code(OK).send({ result: meals })
+    } catch (error) {
+      console.error(error)
+
+      return reply
+        .code(INTERNAL_SERVER_ERROR)
+        .send({ message: 'An error occurred while processing the request.' })
+    }
+  })
+
+  app.get('/summary', { preHandler: checkIfTokenExists }, async (req, reply) => {
+    try {
+      const meals = await knex('meals').select().where({ user_id: req.user?.id })
+
+      const { bestSequence, goodDiet, badDiet } = meals.reduce(
+        (acc, meal) => {
+          if (meal.healthy_diet) {
+            acc.sequence += 1
+            acc.bestSequence = Math.max(acc.bestSequence, acc.sequence)
+            acc.goodDiet += 1
+          } else {
+            acc.sequence = 0
+            acc.badDiet += 1
+          }
+
+          return acc
+        },
+        { goodDiet: 0, badDiet: 0, bestSequence: 0, sequence: 0 }
+      )
+
+      return reply.code(OK).send({
+        total: meals.length,
+        goodDiet,
+        badDiet,
+        bestSequence
+      })
+    } catch (error) {
+      console.error(error)
+
+      return reply
+        .code(INTERNAL_SERVER_ERROR)
+        .send({ message: 'An error occurred while processing the request.' })
+    }
   })
 }
--- src/utils/statusCode.ts
+++ src/utils/statusCode.ts
@@ -1,3 +1,4 @@
 import { StatusCodes } from 'http-status-codes'
 
-export const { OK, CREATED, NOT_FOUND, UNAUTHORIZED, FORBIDDEN, CONFLICT } = StatusCodes
+export const { OK, CREATED, NOT_FOUND, UNAUTHORIZED, FORBIDDEN, CONFLICT, INTERNAL_SERVER_ERROR } =
+  StatusCodes
