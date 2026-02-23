8539a85fa8e992fd8867708b7d92c41bfc16d06d
Demis Russo <demisjunior@hotmail.com>
2026-02-22 22:43:47 -0300

    fix: update PORT type to coerce number in env schema and ensure proper formatting in .env.example

--- src/envs/env.ts
+++ src/envs/env.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 
 const envsSchema = z.object({
 	NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
-	PORT: z.number().default(3333),
+	PORT: z.coerce.number().default(3333),
 	DATABASE_URL: z.string(),
 	JWT_SECRET: z.string()
 })
